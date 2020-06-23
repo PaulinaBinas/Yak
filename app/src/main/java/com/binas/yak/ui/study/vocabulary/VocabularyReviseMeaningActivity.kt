be033b23c186ec637a9447ea5916907b9e657682
PaulinaBinas <binaspaulina1@gmail.com>
2020-06-23 22:23:53 +0200

    Created vocabulary check layout

--- app/src/main/java/com/binas/yak/ui/study/vocabulary/VocabularyReviseMeaningActivity.kt
+++ app/src/main/java/com/binas/yak/ui/study/vocabulary/VocabularyReviseMeaningActivity.kt
@@ -44,4 +44,14 @@ class VocabularyReviseMeaningActivity : AppCompatActivity() {
         startActivity(intent)
         overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom)
     }
+
+    fun onClickSettingsButton(view: View) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom)
+    }
+
+    fun onClickBackButton(view: View) {
+        onBackPressed()
+    }
 }
